667b7b4c5f10f9264e544de139e9435529c60772
Enea <enea@prooffactor.com>
2019-08-21 12:17:59 -0400

    1.0.5

--- src/i18n/ar.js
+++ src/i18n/ar.js
@@ -1,9 +1,9 @@
 var ar = {
-    verified_by: 'أكد عن طريق',
-    aggregate_subtitle_hour: "in the last hour",
-    aggregate_subtitle_day: "in the last day",
-    aggregate_subtitle_week: "in the last week",
-    aggregate_subtitle_month: "in the last month"
+  verified_by: 'أكد عن طريق',
+  aggregate_subtitle_hour: 'in the last hour',
+  aggregate_subtitle_day: 'in the last day',
+  aggregate_subtitle_week: 'in the last week',
+  aggregate_subtitle_month: 'in the last month'
 };
 export default ar;
 //# sourceMappingURL=ar.js.map
--- src/i18n/de.js
+++ src/i18n/de.js
@@ -1,9 +1,9 @@
 var de = {
-    verified_by: 'Verifiziert von',
-    aggregate_subtitle_hour: "in the last hour",
-    aggregate_subtitle_day: "in the last day",
-    aggregate_subtitle_week: "in the last week",
-    aggregate_subtitle_month: "in the last month"
+  verified_by: 'Verifiziert von',
+  aggregate_subtitle_hour: 'in the last hour',
+  aggregate_subtitle_day: 'in the last day',
+  aggregate_subtitle_week: 'in the last week',
+  aggregate_subtitle_month: 'in the last month'
 };
 export default de;
 //# sourceMappingURL=de.js.map
