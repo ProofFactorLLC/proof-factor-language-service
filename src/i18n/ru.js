cfbdcdaa54b8d765db2baf87c3dc152c286a9ca9
Enea <enea@prooffactor.com>
2019-08-21 12:17:47 -0400

    remove webpack config

--- src/i18n/ru.js
+++ src/i18n/ru.js
@@ -1,9 +1,9 @@
 var ru = {
-  verified_by: 'Проверено',
-  aggregate_subtitle_hour: 'in the last hour',
-  aggregate_subtitle_day: 'in the last day',
-  aggregate_subtitle_week: 'in the last week',
-  aggregate_subtitle_month: 'in the last month'
+    verified_by: 'Проверено',
+    aggregate_subtitle_hour: "in the last hour",
+    aggregate_subtitle_day: "in the last day",
+    aggregate_subtitle_week: "in the last week",
+    aggregate_subtitle_month: "in the last month"
 };
 export default ru;
 //# sourceMappingURL=ru.js.map
